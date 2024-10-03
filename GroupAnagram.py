bdcefe34c3cf066aef863e85312307212135d2fa
vignesh1307 <vignesh.shri1307@gmail.com>
2024-10-03 17:00:59 +0530

    bla

--- GroupAnagram.py
+++ GroupAnagram.py
@@ -9,7 +9,7 @@ def group_anagrams(words):
         sorted_word = tuple(sorted(word))
         anagrams[sorted_word].append(word)
     return list(anagrams.values())
-    print("anagrams are :" + anagrams)
+    print("dictionary of anagrams are :" + anagrams)
 
 
 
